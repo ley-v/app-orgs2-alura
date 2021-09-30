e294af167b9b687ba00f7e59c016ae8e656a8875
ley-v <83968941+ley-v@users.noreply.github.com>
2021-09-30 16:38:44 +0100

    Inicio em 2.2 -Utilizando Cards

---
+++ app/src/main/java/br/com/alura/orgs/dao/ProdutosDao.kt
@@ -0,0 +1,21 @@
+package br.com.alura.orgs.dao
+
+import br.com.alura.orgs.model.Produto
+
+class ProdutosDao {
+
+    fun adiciona(produto: Produto) {
+        produtos.add(produto)
+    }
+
+    fun buscaTodos(): List<Produto> {
+        return produtos.toList()
+    }
+
+    companion object {
+        private val produtos = mutableListOf<Produto>(
+            Produto("cobra", "no step on snek", "100.369".toBigDecimal())
+        )
+    }
+
+}
