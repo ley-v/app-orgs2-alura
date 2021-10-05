8d18cfd527b2789039b0140cca4082ad70d40a21
ley-v <83968941+ley-v@users.noreply.github.com>
2021-10-05 19:32:13 +0100

    4.9 salvando imagem na Dialog, Formulário e Lista de Produtos; implementando suporte para gifs

--- app/src/main/java/br/com/alura/orgs/ui/activity/FormularioProdutoActivity.kt
+++ app/src/main/java/br/com/alura/orgs/ui/activity/FormularioProdutoActivity.kt
@@ -1,12 +1,17 @@
 package br.com.alura.orgs.ui.activity
 
+import android.os.Build.VERSION.SDK_INT
 import android.os.Bundle
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-import br.com.alura.orgs.R
 import br.com.alura.orgs.dao.ProdutosDao
 import br.com.alura.orgs.databinding.ActivityFormularioProdutoBinding
+import br.com.alura.orgs.databinding.FormularioImagemBinding
 import br.com.alura.orgs.model.Produto
+import coil.ImageLoader
+import coil.decode.GifDecoder
+import coil.decode.ImageDecoderDecoder
+import coil.load
 import com.google.android.material.textfield.TextInputEditText
 import java.math.BigDecimal
 import java.math.RoundingMode
@@ -16,28 +21,44 @@ class FormularioProdutoActivity : AppCompatActivity() {
     private val binding by lazy {
         ActivityFormularioProdutoBinding.inflate(layoutInflater)
     }
-
+    private var url: String? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         configuraBotaoSalvar()
-    }
 
-    private fun configuraBotaoSalvar() {
-        val botaoSalvar = binding.activityFormularioProdutoBotaoSalvar
-        val dao = ProdutosDao()
+        val imageLoader = ImageLoader.Builder(this)
+            .componentRegistry {
+                if (SDK_INT >= 28) {
+                    add(ImageDecoderDecoder(this@FormularioProdutoActivity))
+                } else {
+                    add(GifDecoder())
+                }
+            }
+            .build()
+
+        val bindingFormularioImagem = FormularioImagemBinding.inflate(layoutInflater)
+        bindingFormularioImagem.formularioImagemBotaoCarregar.setOnClickListener {
+            url = bindingFormularioImagem.formularioImagemUrl.text.toString()
+            bindingFormularioImagem.formularioImagemImageview.load(url, imageLoader = imageLoader)
+        }
 
         binding.activityFormularioProdutoImagem.setOnClickListener {
             AlertDialog.Builder(this)
-                .setView(R.layout.formulario_imagem)
+                .setView(bindingFormularioImagem.root)
                 .setPositiveButton("Confirmar") { _, _ ->
-
+                    binding.activityFormularioProdutoImagem.load(url, imageLoader)
                 }
                 .setNegativeButton("Cancelar") { _, _ ->
 
                 }
                 .show()
         }
+    }
+
+    private fun configuraBotaoSalvar() {
+        val botaoSalvar = binding.activityFormularioProdutoBotaoSalvar
+        val dao = ProdutosDao()
 
         botaoSalvar.setOnClickListener {
             val produtoNovo = criaProduto()
@@ -47,8 +68,7 @@ class FormularioProdutoActivity : AppCompatActivity() {
     }
 
     private fun criaProduto(): Produto {
-//        val campoNome2 = findViewById<TextInputEditText>(R.id.activity_formulario_produto_nome)
-//        val nome = campoNome2.text.toString()
+//        val campoNome2 = findViewById<TextInp
 
         val campoNome: TextInputEditText = binding.activityFormularioProdutoNome
         val nome = campoNome.text.toString()
@@ -63,10 +83,10 @@ class FormularioProdutoActivity : AppCompatActivity() {
         }
 
         return Produto(
-//            nome, "xxt", BigDecimal.ZERO
             nome = nome,
             descricao = descricao,
-            valor = valor
+            valor = valor,
+            imagem = url
         )
     }
 
--- app/src/main/java/br/com/alura/orgs/ui/recyclerview/adapter/ListaProdutosAdapter.kt
+++ app/src/main/java/br/com/alura/orgs/ui/recyclerview/adapter/ListaProdutosAdapter.kt
@@ -1,11 +1,15 @@
 package br.com.alura.orgs.ui.recyclerview.adapter
 
 import android.content.Context
+import android.os.Build
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import br.com.alura.orgs.databinding.ProdutoItemBinding
 import br.com.alura.orgs.model.Produto
+import coil.ImageLoader
+import coil.decode.GifDecoder
+import coil.decode.ImageDecoderDecoder
 import coil.load
 import java.math.BigDecimal
 import java.text.NumberFormat
@@ -18,8 +22,20 @@ class ListaProdutosAdapter(
 
     private val produtos = produtos.toMutableList()
 
-    class ViewHolder(private val binding: ProdutoItemBinding) :
+
+
+    inner class ViewHolder(private val binding: ProdutoItemBinding) :
         RecyclerView.ViewHolder(binding.root) {
+
+        var imageLoader = ImageLoader.Builder(context)
+            .componentRegistry {
+                if (Build.VERSION.SDK_INT >= 28) {
+                    add(ImageDecoderDecoder(context))
+                } else {
+                    add(GifDecoder())
+                }
+            }
+            .build()
 
         fun vincula(produto: Produto) {
             val nome = binding.produtoItemNome
@@ -30,7 +46,7 @@ class ListaProdutosAdapter(
             val valorEmMoeda = formataParaMoedaEuro(produto.valor)
             valor.text = valorEmMoeda
             val imagem = binding.imageView
-            imagem.load("https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2F1.bp.blogspot.com%2F-Mpv5D-2K11w%2FYT4nVOJRLoI%2FAAAAAAAABGQ%2Fq1WWyqPdmc4jOAaWLrpd5rNQWgjDtapWACNcBGAsYHQ%2Fs0%2Fdff3de9a7f643c50f38da8e266c257fb1628748785_full.jpg&f=1&nofb=1")
+            imagem.load(produto.imagem, imageLoader)
         }
 
         private fun formataParaMoedaEuro(valor: BigDecimal): String? {
